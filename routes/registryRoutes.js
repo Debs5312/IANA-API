e98bfb06a8261d03d3ad6b2770f8d24bf4766267
Debs5312 <debojyotisaha5@gmail.com>
2025-12-02 18:50:25 +0530

    Revert "authentication middleware in IANA-API"
    This reverts commit 8952b70cf0d7385e9da1df7698d21292e918feb4.

--- routes/registryRoutes.js
+++ routes/registryRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const { getRegistry, getLanguageRegistry, createConceptHandler, fetchConceptHandler, deleteConceptHandler } = require('../controllers/registryController');
-const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/registry', getRegistry);
 router.get('/registry/language', getLanguageRegistry);
-router.get('/concepts', authenticateToken, fetchConceptHandler);
+router.get('/concepts', fetchConceptHandler);
 // router.post('/createConceptScheme', createConceptSchemeHandler);
-router.post('/createConcept', authenticateToken, createConceptHandler);
-router.delete('/deleteConcept', authenticateToken, deleteConceptHandler);
+router.post('/createConcept', createConceptHandler);
+router.delete('/deleteConcept', deleteConceptHandler);
 
 
 module.exports = router;
