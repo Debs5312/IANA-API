8952b70cf0d7385e9da1df7698d21292e918feb4
Debs5312 <debojyotisaha5@gmail.com>
2025-12-02 18:41:19 +0530

    authentication middleware in IANA-API

--- routes/registryRoutes.js
+++ routes/registryRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { getRegistry, getLanguageRegistry, createConceptHandler, fetchConceptHandler, deleteConceptHandler } = require('../controllers/registryController');
+const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/registry', getRegistry);
 router.get('/registry/language', getLanguageRegistry);
-router.get('/concepts', fetchConceptHandler);
+router.get('/concepts', authenticateToken, fetchConceptHandler);
 // router.post('/createConceptScheme', createConceptSchemeHandler);
-router.post('/createConcept', createConceptHandler);
-router.delete('/deleteConcept', deleteConceptHandler);
+router.post('/createConcept', authenticateToken, createConceptHandler);
+router.delete('/deleteConcept', authenticateToken, deleteConceptHandler);
 
 
 module.exports = router;
